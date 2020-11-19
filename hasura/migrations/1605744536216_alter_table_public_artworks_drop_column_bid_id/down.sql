f94e309aaba92a8bc1dbfe6dfcd2e745696da0e9
Adam Soltys <asoltys@gmail.com>
2020-11-19 13:47:28 -0500

    remove duplicate migrations

--- hasura/migrations/1605744536216_alter_table_public_artworks_drop_column_bid_id/down.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "public"."artworks" ADD COLUMN "bid_id" uuid;
-ALTER TABLE "public"."artworks" ALTER COLUMN "bid_id" DROP NOT NULL;
-ALTER TABLE "public"."artworks" ADD CONSTRAINT artworks_bid_id_fkey FOREIGN KEY (bid_id) REFERENCES "public"."transactions" (id) ON DELETE restrict ON UPDATE restrict;
--- hasura/migrations/1605744587830_run_sql_migration/up.sql
+++
@@ -1,1 +0,0 @@
-DROP FUNCTION public.artwork_bidder;
