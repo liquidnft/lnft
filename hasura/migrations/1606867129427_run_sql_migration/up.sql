7865936ac166bd468500f22651f93a66e2afd164
Adam Soltys <asoltys@gmail.com>
2020-12-01 15:59:18 -0800

    check tx type field

---
+++ hasura/migrations/1606867129427_run_sql_migration/up.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE FUNCTION public.artwork_bid2(artwork_row artworks)
+ RETURNS transactions
+ LANGUAGE sql
+ STABLE
+AS $function$
+    SELECT transactions.*
+    FROM transactions
+    JOIN artworks ON transactions.artwork_id = artworks.id
+    WHERE transactions.artwork_id = artwork_row.id
+    AND transactions.type = 'bid'
+    AND (transactions.created_at > artworks.transferred_at
+    OR artworks.transferred_at IS NULL)
+    ORDER BY amount DESC
+    LIMIT 1
+$function$;
