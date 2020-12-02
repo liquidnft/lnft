6660357d5513da356b19f78e4a5f5a13b289b5eb
Adam Soltys <asoltys@gmail.com>
2020-12-01 16:04:54 -0800

    fix collectors query

--- src/queries/users.js
+++ src/queries/users.js
@@ -2,13 +2,14 @@ import decode from "jwt-decode";
 import { operationStore, query } from "@urql/svelte";
 
 let fields =
-  "id, username, location, bio, email, full_name, website, avatar_url, balance";
+  "id, username, location, bio, email, full_name, website, avatar_url";
 
 let computed = "followed, num_follows, num_followers";
 
 export const get = (id) => `subscription {
   users_by_pk(id: "${id}") { 
     ${fields} 
+    balance
     ${computed}
   }
 }`;
@@ -17,6 +18,7 @@ export const update = {
   query: `mutation update_user($user: users_set_input!, $id: uuid!) {
     update_users_by_pk(pk_columns: { id: $id }, _set: $user) {
       ${fields}
+      balance
       ${computed}
     }
   }`,
