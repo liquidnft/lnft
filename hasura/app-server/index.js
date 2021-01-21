b89b4537e71b653054827c31c52d24ad73853773
Adam Soltys <asoltys@gmail.com>
2021-01-20 20:59:21 -0800

    app-server

---
+++ hasura/app-server/index.js
@@ -0,0 +1,18 @@
+// Require the framework and instantiate it
+const fastify = require('fastify')({
+  logger: true
+})
+
+// Declare a route
+fastify.get('/', function (request, reply) {
+  reply.send({ hello: 'world' })
+})
+
+// Run the server!
+fastify.listen(3000, '0.0.0.0', function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  fastify.log.info(`server listening on ${address}`)
+})
---
+++ hasura/migrations/1611204106369_alter_table_public_users_add_column_amp_mnemonic/up.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "public"."users" ADD COLUMN "amp_mnemonic" text NULL;
