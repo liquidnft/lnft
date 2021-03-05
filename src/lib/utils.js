946020d61912bb1d0c74ba01d5152b90d5d60d7e
Adam Soltys <asoltys@gmail.com>
2021-03-05 09:03:41 -0800

    number format, trim trailing zeros

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -81,7 +81,7 @@ const units = (asset) => {
   if (tickers[asset]) ({ decimals, precision } = tickers[asset]);
   return [
     (val) => Math.round(val * 10 ** precision),
-    (sats) => (sats / 10 ** precision).toFixed(decimals),
+    (sats) => format(sats, decimals),
     ticker(asset),
   ];
 };
@@ -165,6 +165,31 @@ const fullscreen = (elem) => {
 
   full.set(true);
 };
+
+
+function format(n, p) {
+  if (!parseInt(p)) return parseInt(n).toFixed(0);
+  else {
+    let x = n / 10 ** p;
+    if (Math.abs(x) < 1.0) {
+      var e = parseInt(x.toString().split('e-')[1]);
+      if (e) {
+        x *= Math.pow(10, e - 1);
+        x = x.toFixed(9 - e);
+        x = '0.' + new Array(e).join('0') + x.toString().substring(2);
+      }
+    } else {
+      var e = parseInt(x.toString().split('+')[1]);
+      if (e > 20) {
+        e -= 20;
+        x /= Math.pow(10, e);
+        x += new Array(e + 1).join('0');
+      }
+    }
+    if (x.toString().split('.')[1] > p && x.toFixed) return parseFloat(x.toFixed(p)).toString();
+    return x;
+  }
+}
 
 export {
   addressLabel,
