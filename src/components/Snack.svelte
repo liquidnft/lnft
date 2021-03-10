5213093cdb5044c26c91c6d00ce44054b2d331d2
Adam Soltys <asoltys@gmail.com>
2021-03-10 09:25:25 -0800

    snack style

--- src/components/Snack.svelte
+++ src/components/Snack.svelte
@@ -20,11 +20,11 @@
     @apply fixed z-20 mt-6;
     width: 300px;
     right: 10%;
-    top: 10%;
+    top: 3em;
   }
 
   .snack {
-    @apply px-4 py-3 rounded relative mb-4 mx-auto font-bold bg-white;
+    @apply relative w-full flex p-4 rounded;
   }
 
   .snack.info {
@@ -37,9 +37,9 @@
 </style>
 
 {#if $snack}
-  <div class="snack-container">
-    <div class="snack mx-0 w-full flex"  class:info class:error>
-      <div class="flex-grow">{$snack.msg}</div>
+  <div class="snack-container flex">
+    <div class="snack"  class:info class:error>
+      <div class="flex-grow mr-2">{$snack.msg}</div>
       <button class="ml-auto my-auto w-auto" on:click={() => $snack = undefined}><i class="fa fa-times text-2xl" /></button>
     </div>
   </div>
