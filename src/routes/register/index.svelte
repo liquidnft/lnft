76384eda3c94466cbddad6897ec4d0a400a26a38
Adam Soltys <asoltys@gmail.com>
2021-03-14 23:59:33 -0700

    sign in link

--- src/routes/register/index.svelte
+++ src/routes/register/index.svelte
@@ -73,50 +73,49 @@
   <form class="mb-6" on:submit|preventDefault={submit} autocomplete="off">
     {#if loading}
       <ProgressLinear />
+    {:else if registered}
+      <h2 class="mb-8">Registered!</h2>
+      <p>
+        Thanks for registering. Please check your email for an activation link.
+      </p>
+
+      <p class="mt-4">
+        <a href="/login" class="secondary-color">Continue to sign in page</a>
+      </p>
     {:else}
+      <h2 class="mb-8">Sign up</h2>
+      <div class="flex flex-col mb-4">
+        <label
+          class="mb-2 font-medium text-gray-600"
+          for="first_name">Email</label>
+        <input placeholder="Email" bind:value={email} bind:this={ref} />
+      </div>
+      <div class="flex flex-col mb-4">
+        <label
+          class="mb-2 font-medium text-gray-600"
+          for="first_name">Username</label>
+        <input placeholder="Username" bind:value={username} />
+      </div>
+      <div class="flex flex-col mb-4">
+        <label
+          class="mb-2 font-medium text-gray-600"
+          for="last_name">Password</label>
+        <input
+          placeholder="At least 8 characters."
+          type="password"
+          bind:value={password} />
+      </div>
+      <span class="block w-full">By signing up, you agree to the
+        <a href="/terms-and-conditions" class="text-midblue">Terms and
+          Conditions</a>
+        and
+        <a href="/privacy-policy" class="text-midblue">Privacy Policy</a></span>
+      <div class="flex my-5 justify-end">
+        <button class="primary-btn w-1/2" type="submit">Register</button>
+      </div>
 
-      {#if registered}
-        <h2 class="mb-8">Registered!</h2>
-        <p>
-          Thanks for registering. Please check your email for an activation
-          link.
-        </p>
-
-        <p class="mt-4">
-          <a href="/login" class="secondary-color">Continue to sign in page</a>
-        </p>
-      {:else}
-        <h2 class="mb-8">Sign up</h2>
-        <div class="flex flex-col mb-4">
-          <label
-            class="mb-2 font-medium text-gray-600"
-            for="first_name">Email</label>
-          <input placeholder="Email" bind:value={email} bind:this={ref} />
-        </div>
-        <div class="flex flex-col mb-4">
-          <label
-            class="mb-2 font-medium text-gray-600"
-            for="first_name">Username</label>
-          <input placeholder="Username" bind:value={username} />
-        </div>
-        <div class="flex flex-col mb-4">
-          <label
-            class="mb-2 font-medium text-gray-600"
-            for="last_name">Password</label>
-          <input
-            placeholder="At least 8 characters."
-            type="password"
-            bind:value={password} />
-        </div>
-        <span class="block w-full">By signing up, you agree to the
-          <a href="/terms-and-conditions" class="text-midblue">Terms and
-            Conditions</a>
-          and
-          <a href="/privacy-policy" class="text-midblue">Privacy Policy</a></span>
-        <div class="flex my-5 justify-end">
-          <button class="primary-btn w-1/2" type="submit">Register</button>
-        </div>
-      {/if}
+      <a href="/login" class="text-midblue">
+        Already have an account? Sign in</a>
     {/if}
   </form>
 </div>
