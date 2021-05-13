c172eba75524e706f77f7731d1460a25a74986f6
Adam Soltys <asoltys@gmail.com>
2021-05-13 12:17:31 -0700

    3 column grid layout

--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -12,7 +12,7 @@
   let w;
   let hidden;
   let maxPages = 7;
-  $: columns = w >= 1232 ? 4 : 3;
+  $: columns = 3;
 
   $: offset =
     artworks &&
@@ -51,14 +51,14 @@
   
 </style>
 
-<div class="flex flex-wrap" bind:clientWidth={w}>
+<div class="grid gap-10 grid-cols-3" bind:clientWidth={w}>
   {#each artworks as artwork, i (artwork.id)}
     {#if i % offset === 0}
-      <div class="w-full flex" class:invisible={i === 0} class:h-0={i === 0}>
-        <h4 class="mx-auto mb-12" id={`artwork-${i}`}>{i / offset + 1}</h4>
+      <div class="col-span-3 w-full flex invisible h-0">
+        <h4 class="mx-auto" id={`artwork-${i}`}>{i / offset + 1}</h4>
       </div>
     {/if}
-    <div class="market-gallery w-full md:w-1/2 sm:pr-10 lg:w-1/3 xl:w-1/4 mb-20">
+    <div class="market-gallery w-full mb-20">
       {#if artwork}
         {#if !loaded[artwork.id]}
           <LoadingPlaceholder />
