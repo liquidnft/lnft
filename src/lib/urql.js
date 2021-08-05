74662a44b547e6dbaf37720885c482c8f6cc6201
Esky33 <djescaba@me.com>
2021-08-05 18:40:59 +0100

    Update urql.js

--- src/lib/urql.js
+++ src/lib/urql.js
@@ -22,8 +22,8 @@ if (import.meta && import.meta.env && import.meta.env !== "production") {
   url = import.meta.env.SNOWPACK_PUBLIC_HTTP;
   wsUrl = import.meta.env.SNOWPACK_PUBLIC_WS;
 } else {
-  url = "https://bid2.nftglee.com/v1/graphql";
-  wsUrl = "wss://bid2.nftglee.com/v1/graphql";
+  url = "https://junglelab.io/v1/graphql";
+  wsUrl = "wss://junglelab.io/v1/graphql";
 }
 
 export const setupUrql = async () => {
