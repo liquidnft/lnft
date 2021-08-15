1de86d5314fccadbff989f01f76ae8ab7501fc0a
Esky33 <30862925+Eskyee@users.noreply.github.com>
2021-08-15 04:10:56 +0100

    local dev config

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -152,11 +152,11 @@
 <div class="flex header-container mx-auto justify-center marg-bottom">
   <div class="header text-center">
     <h1 class="text-left md:text-center md:w-full">
-      JungleLab
+      JUNGLELAB NFTs
       <br />music art video
     </h1>
     <h5 class="md:max-w-lg mx-auto text-left md:text-center">
-      Upload, collect, and transact exclusive rare digital lifestyle music art video on the Liquid Network
+      Upload, collect, and transact exclusive rare digital music art video on the Bitcoin Liquid Network
     </h5>
     <a class="primary-btn" href={`/market`}>Start exploring</a>
   </div>
