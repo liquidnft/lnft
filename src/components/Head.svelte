13343a58e6e78efb236f640c0e95ad527aae68b8
Ihor Brazhnichenko <ihor.bra@gmail.com>
2021-08-18 16:44:16 +0300

    Replace hardcoded branding for Head (replace meta)

--- src/components/Head.svelte
+++ src/components/Head.svelte
@@ -1,24 +1,24 @@
 <script>
   import { art } from "$lib/store";
-
+  import branding from "$lib/branding";
 </script>
 
 <svelte:head>
-  <meta name="twitter:card" content="summary_large_image" />
-  <meta name="twitter:site" content="@raretoshi" />
-  <meta name="twitter:creator" content="@raretoshi" />
+  <meta name="twitter:card" content={branding.meta.general.twitter.card} />
+  <meta name="twitter:site" content={branding.meta.general.twitter.site} />
+  <meta name="twitter:creator" content={branding.meta.general.twitter.creator} />
 
   {#if $art}
-    <meta property="og:title" content={`Raretoshi - ${$art.title}`} />
-    <meta property="og:image" content={`/api/ipfs/${$art.filename}`} />
-    <meta property="og:url" content={`https://raretoshi.com/a/${$art.slug}`} />
-    <meta name="twitter:title" content={`Raretoshi - ${$art.title}`} />
-    <meta name="twitter:image" content={`/api/ipfs/${$art.filename}`} />
+    <meta property="og:title" content={branding.meta.artPage($art).og.title} />
+    <meta property="og:image" content={branding.meta.artPage($art).og.image} />
+    <meta property="og:url" content={branding.meta.artPage($art).og.url} />
+    <meta name="twitter:title" content={branding.meta.artPage($art).twitter.title} />
+    <meta name="twitter:image" content={branding.meta.artPage($art).twitter.image} />
   {:else}
-    <meta property="og:title" content="Raretoshi" />
-    <meta property="og:image" content="https://raretoshi.com/splash.png" />
-    <meta property="og:url" content="https://raretoshi.com/" />
-    <meta name="twitter:title" content="Raretoshi" />
-    <meta name="twitter:image" content="https://raretoshi.com/splash.png" />
+    <meta property="og:title" content={branding.meta.general.og.title} />
+    <meta property="og:image" content={branding.meta.general.og.image} />
+    <meta property="og:url" content={branding.meta.general.og.url} />
+    <meta name="twitter:title" content={branding.meta.general.twitter.title} />
+    <meta name="twitter:image" content={branding.meta.general.twitter.image} />
   {/if}
 </svelte:head>
