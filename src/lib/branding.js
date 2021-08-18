5506ed22436e3662620e1749bcff3e8cabbf594e
Ihor Brazhnichenko <ihor.bra@gmail.com>
2021-08-18 14:39:11 +0300

    Replace hardcoded branding (project name + support email + urls) on Privacy Policy Page

--- src/lib/branding.js
+++ src/lib/branding.js
@@ -1,6 +1,12 @@
 const BRANDING = {
   projectName: 'Raretoshi',
   
+  urls: {
+    base: 'raretoshi.com',
+    www: 'www.raretoshi.com',
+    protocol: 'https://raretoshi.com',
+  },
+  
   emails: {
     support : 'support@raretoshi.com'
   }
