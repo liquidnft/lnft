eff94a1d5cc2d4fca0d44cd2e647093ee218ad46
Ihor Brazhnichenko <ihor.bra@gmail.com>
2021-09-14 17:39:32 +0300

    #styleguide Remove margin top for the footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -20,7 +20,7 @@
 }
 </style>
 
-<div class="bg-black px-2 mt-24 colored-border relative">
+<div class="bg-black px-2 colored-border relative">
   <div class="container footer mx-auto flex flex-wrap text-white pt-12">
     <div
       class="flex flex-col text-sm w-full lg:w-2/4 mb-10 order-last lg:order-first">
