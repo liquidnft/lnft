5b3ea240a542d7e3d2fefa7564b58f66462691bc
Ihor Brazhnichenko <ihor.bra@gmail.com>
2021-09-20 01:24:47 +0300

    #navbar Provide new tailwind breakpoints for @screen mixin to be able to use from 0 to X endpoints

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,13 +9,17 @@ module.exports = {
       'md': '768px',
       'lg': '1024px',
       'xl': '1280px',
+      'till-sm': {max:'640px'},
+      'till-md': {max:'768px'},
+      'till-lg': {max:'1024px'},
+      'till-xl': {max:'1280px'},
     },
     extend: {
       colors: {
         primary: "#8feef5",
         secondary: "#3ba5ac",
-      } 
-    } 
+      }
+    }
   },
   variants: {
     borderWidth: ["responsive", "hover", "focus"],
