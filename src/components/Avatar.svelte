f3d4cf0f0d9e019e40e750180816aa2871e54536
Ihor Brazhnichenko <ihor.bra@gmail.com>
2021-09-21 15:14:30 +0300

    #card Add simple attribute to Avatar to display img without wrapped divs

--- src/components/Avatar.svelte
+++ src/components/Avatar.svelte
@@ -3,7 +3,7 @@
   export let src = undefined;
   export let overlay = undefined;
   export let size = "small";
-
+  export let simple = false;
 </script>
 
 <style>
@@ -21,22 +21,35 @@
 
 </style>
 
-<div class={`${size} my-auto relative`}>
-  <div
-    class={`relative ${size} group rounded-full overflow-hidden shadow-inner text-center cursor-pointer`}>
-    {#if user || src}
-      <img
-        key={user && user.username}
-        src={user ? `/api/ipfs/${user.avatar_url}` : src.startsWith('data') ? src : `/api/ipfs/${src}`}
-        alt={user ? user.username : 'lovely avatar'}
+{#if !!simple}
+
+  <img
+      key={user && user.username}
+      src={user ? `/api/ipfs/${user.avatar_url}` : src.startsWith('data') ? src : `/api/ipfs/${src}`}
+      alt={user ? user.username : 'lovely avatar'}
+      class="absolute w-full h-full object-cover object-center visible group-hover:hidden overflow-hidden" />
+
+{:else}
+
+  <div class={`${size} my-auto relative`}>
+    <div
+        class={`relative ${size} group rounded-full overflow-hidden shadow-inner text-center cursor-pointer`}>
+      {#if user || src}
+        <img
+            key={user && user.username}
+            src={user ? `/api/ipfs/${user.avatar_url}` : src.startsWith('data') ? src : `/api/ipfs/${src}`}
+            alt={user ? user.username : 'lovely avatar'}
         class="absolute w-full h-full object-cover object-center visible overflow-hidden" />
+      {/if}
+    </div>
+    {#if overlay}
+      <img
+      alt="Multisig"
+          src={overlay}
+          class="w-6 h-6 absolute"
+          style="bottom: -8px; right: -20px" />
     {/if}
   </div>
-  {#if overlay}
-    <img
-      alt="Multisig"
-      src={overlay}
-      class="w-6 h-6 absolute"
-      style="bottom: -8px; right: -20px" />
-  {/if}
-</div>
+
+
+{/if}
