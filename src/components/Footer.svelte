ffbc44336da231060dd8445aaae2320a9dfb66d4
Ihor Brazhnichenko <ihor.bra@gmail.com>
2021-09-21 15:05:07 +0300

    #footer Footer fixes

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -23,9 +23,18 @@
 <div class="bg-black px-2 colored-border relative">
   <div class="container footer mx-auto flex flex-wrap text-white pt-12">
     <div
-      class="flex flex-col text-sm w-full lg:w-2/4 mb-10 order-last lg:order-first">
-      <a href="/"><Logo class="w-24"/></a>
-      <p class="mt-4">&copy;2021 {branding.projectName}</p>
+      class="flex flex-col text-sm w-full lg:w-2/4 mb-10 order-last lg:order-first p-2">
+      <div class="flex items-center">
+        <div>
+          <a href="/"><Logo class="w-24"/></a>
+          <p class="mt-2 text-sm">&copy;2021 {branding.projectName}</p>
+        </div>
+        <div class="border-l border-gray-800 pl-4 ml-4 h-full items-center flex">
+          <div>
+            The only marketplace for <br/><p class="font-bold">NFT experiences</p>
+          </div>
+        </div>
+      </div>
       <div class="flex items-center mt-12">
         <p class="pr-3">Powered by:</p>
         <img src="/liquid_logo.svg" alt="liquid network logo" />
