035a1e32bee4e94859a7a550d9e9d9b20513a3aa
Ihor Brazhnichenko <ihor.bra@gmail.com>
2021-09-22 12:58:06 +0300

    #wallet Add palette to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,32 @@
+const COLORS = {
+  BLUE: {
+    DEFAULT: '#335FE3',
+    '50': '#FDFDFF',
+    '100': '#E6ECFC',
+    '200': '#BAC9F5',
+    '300': '#8DA5EF',
+    '400': '#6082E9',
+    '500': '#335FE3',
+    '600': '#1B46C8',
+    '700': '#15379B',
+    '800': '#0F276E',
+    '900': '#091741'
+  },
+  ORANGE: {
+    DEFAULT: '#F77030',
+    '50': '#FFFFFF',
+    '100': '#FFF8F4',
+    '200': '#FDD6C3',
+    '300': '#FBB492',
+    '400': '#F99261',
+    '500': '#F77030',
+    '600': '#EB5209',
+    '700': '#BA4107',
+    '800': '#893005',
+    '900': '#581E03'
+  }
+};
+
 module.exports = {
   mode: 'jit',
   purge: [
@@ -15,9 +44,17 @@ module.exports = {
       'till-xl': {max:'1280px'},
     },
     extend: {
+      backgroundColor: {
+        blue: COLORS.BLUE,
+        orange: COLORS.ORANGE,
+      },
       colors: {
         primary: "#8feef5",
         secondary: "#3ba5ac",
+        // blue (base - #335fe3)
+        blue: COLORS.BLUE,
+        // orange (base - #f77030)
+        orange: COLORS.ORANGE,
       }
     }
   },
