6cd5be23b4237d8f60f84d1cf36296a7f3030614
Adam Soltys <asoltys@gmail.com>
2021-10-25 12:30:07 -0700

    use env vars

--- src/lib/wallet.js
+++ src/lib/wallet.js
@@ -42,7 +42,8 @@ const SERVER_PUBKEY = Buffer.from(
   "03c3722bb4260f8c449fc8f266a58348d99410a26096fba84fb15c1d66d868f87b",
   "hex"
 );
-const network = networks.regtest;
+
+const network = networks[import.meta.env.VITE_NETWORK];
 
 const singleAnyoneCanPay =
   Transaction.SIGHASH_SINGLE | Transaction.SIGHASH_ANYONECANPAY;
