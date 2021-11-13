bfc45dcb806e3f84f302c7bd3b1c8a6f81c814e5
Gyulai Zoltan <gyulainagyzoltan@gmail.com>
2021-11-13 14:55:14 +0200

    feat: added env base url

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,9 +3,12 @@ import { token } from "$lib/store";
 import { get } from "svelte/store";
 import { err } from "$lib/utils";
 
-export const api = wretch().url("/api");
-export const electrs = wretch().url("/api/el");
-export const hasura = wretch().url("/api/v1/graphql");
+const baseUrl = import.meta.env.VITE_BASE_URL || '';
+
+export const api = wretch().url(`${baseUrl}/api`);
+export const electrs = wretch().url(`${baseUrl}/api/el`);
+export const hasura = wretch().url(`${baseUrl}/api/v1/graphql`);
+
 export const pub = (t) => (t ? hasura.auth(`Bearer ${t}`) : hasura);
 export const query = async (query, variables) => {
   let { data, errors } = await pub(get(token)).post({ query, variables }).json();
