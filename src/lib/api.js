741b5722c47e621a98d0f27f272938f15ed9be04
Zoltan <gyulainagyzoltan@gmail.com>
2021-11-18 19:53:12 +0200

    fix: env api url support for local env

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,15 +3,17 @@ import { token } from "$lib/store";
 import { get } from "svelte/store";
 import { err } from "$lib/utils";
 
-const baseUrl = import.meta.env.VITE_BASE_URL || '';
+const baseUrl = import.meta.env.VITE_BASE_URL || "";
 
-export const api = wretch().url(`${baseUrl}/api`);
-export const electrs = wretch().url(`${baseUrl}/api/el`);
-export const hasura = wretch().url(`${baseUrl}/api/v1/graphql`);
+export const api = wretch().url(`${baseUrl}`);
+export const electrs = wretch().url(`${baseUrl}/el`);
+export const hasura = wretch().url(`${baseUrl}/v1/graphql`);
 
 export const pub = (t) => (t ? hasura.auth(`Bearer ${t}`) : hasura);
 export const query = async (query, variables) => {
-  let { data, errors } = await pub(get(token)).post({ query, variables }).json();
+  let { data, errors } = await pub(get(token))
+    .post({ query, variables })
+    .json();
   if (errors) throw new Error(errors[0].message);
   return data;
 };
