006f253175272e768616a9458a3944fbabd0ecfb
Ihor Brazhnichenko <ihor.bra@gmail.com>
2021-11-18 18:56:17 +0200

    Dropzone: update the design

--- src/components/Dropzone.svelte
+++ src/components/Dropzone.svelte
@@ -34,70 +34,36 @@
 
 </script>
 
-<style>
-  #fileElem {
-    position: fixed;
-    top: -100em;
-  }
-
-  #drop-area {
-    cursor: pointer;
-    border: 1px solid gray;
-    border-radius: 10px;
-    width: 80%;
-    margin-top: 30px;
-  }
-
-  form {
-    height: 260px;
-  }
-
-  #drop-area.highlight {
-    border-color: black;
-  }
-
-  #fileElem {
-    position: fixed;
-    top: -100em;
-  }
-
-  @media only screen and (max-width: 800px) {
-    #drop-area {
-      width: 100%;
-      max-width: 100%;
-      margin: 0;
-      margin-top: 50px;
-    }
-  }
-
-</style>
 
 {#if style === 'box'}
   <div
-    id="drop-area"
-    on:click={open}
-    on:dragenter={start}
-    on:dragover={start}
-    on:dragleave={stop}
-    on:focus={start}
-    on:mouseover={start}
-    on:blur={stop}
-    on:mouseout={stop}
-    on:drop={drop}
-    class:highlight class="w-full m-0">
-    <form class="text-center w-full">
-      <div class="flex justify-center flex-col align-center h-full">
-        <span class="text-gray-400 mb-8">{title}</span>
-        <button class="primary-btn mx-auto w-36">Choose file</button>
+      id="drop-area"
+      on:dragenter={start}
+      on:dragover={start}
+      on:dragleave={stop}
+      on:focus={start}
+      on:mouseover={start}
+      on:blur={stop}
+      on:mouseout={stop}
+      on:drop={drop}
+      class:highlight>
+
+    <div class="text-black">
+      <div class="mt-4 h-44 rounded-md border-gray-300 border flex flex-col justify-center items-center relative p-4">
+        <div>PNG, GIF, MP4. Max 100mb</div>
+        <input
+            bind:this={fileInput}
+            type="file"
+            id="fileElem"
+            multiple
+            accept="image/*,video/*"
+            class="absolute opacity-0 w-full h-full top-0 left-0 right-0 bottom-0 cursor-pointer"
+            on:change={(e) => dispatch('file', e.target.files[0])}
+        />
+
+        <div class="rounded-full bg-gray-200 px-4 py-2 cursor-pointer text-sm color-gray-500 mt-4">Choose file</div>
       </div>
-      <input
-        bind:this={fileInput}
-        type="file"
-        id="fileElem"
-        multiple
-        accept="image/*,video/*"
-        on:change={(e) => dispatch('file', e.target.files[0])} />
-    </form>
+    </div>
   </div>
 {:else}
   <a
