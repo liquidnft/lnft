1321953faa4f8cb1bf5a234c47656559f8b4a0c1
Ihor Brazhnichenko <ihor.bra@gmail.com>
2021-11-18 18:55:09 +0200

    Add Textarea & Input elements to styleguide

---
+++ src/styleguide/components/Input.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let type = 'text';
+  export let placeholder = null;
+  export let value = '';
+</script>
+
+<input type="text" bind:value {placeholder} class="text-sm appearance-none h-12 rounded-md bg-gray-100 border border-gray-300 mt-2 w-full"/>
+
---
+++ src/styleguide/components/Textarea.svelte
@@ -0,0 +1,6 @@
+<script>
+  export let placeholder = null;
+  export let value;
+</script>
+
+<textarea bind:value {placeholder} class="text-sm appearance-none h-48 rounded-md bg-gray-100 border border-gray-300 mt-2 w-full"></textarea>
