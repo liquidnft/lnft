b81b0b0c239fc70c4e1eff83d469e0c03ea04f9b
Marius Buraga <marius.buraga89@yahoo.com>
2021-11-19 23:32:51 +0200

    Favourite action fix

--- src/components/Heart.svelte
+++ src/components/Heart.svelte
@@ -2,6 +2,8 @@
   import { user } from "$lib/store";
   import { createFavorite, deleteFavorite } from "$queries/favorites";
   import { requireLogin } from "$lib/auth";
+  import { err } from "$lib/utils";
+  import { query } from "$lib/api";
 
   import Fa from "svelte-fa";
   import { faHeart } from "@fortawesome/free-regular-svg-icons";
@@ -13,35 +15,32 @@
 
   let favorite = async () => {
     try {
-    await requireLogin();
-    let { id: artwork_id } = artwork;
-    let { id: user_id } = $user;
+      await requireLogin();
+      let { id: artwork_id } = artwork;
+      let { id: user_id } = $user;
 
-    if (favorited) {
-      await query(deleteFavorite, { artwork_id, user_id });
-      artwork.num_favorites--;
-      favorited = false;
-    } else {
-      createFavorite({ artwork_id });
-      await query(createFavorite, { artwork_id });
-      artwork.num_favorites++;
-      favorited = true;
-    }
-    } catch(e) {
+      if (favorited) {
+        await query(deleteFavorite, { artwork_id, user_id });
+        artwork.num_favorites--;
+        favorited = false;
+      } else {
+        await query(createFavorite, { artwork_id });
+        artwork.num_favorites++;
+        favorited = true;
+      }
+    } catch (e) {
       err(e);
-    } 
+    }
   };
-
 </script>
+
+<div on:click={favorite} class:favorited>
+  <Fa icon={favorited ? solidHeart : faHeart} size="1.5x" />
+</div>
 
 <style>
   div:hover,
   .favorited {
     @apply text-primary cursor-pointer;
   }
-
 </style>
-
-<div on:click={favorite} class:favorited>
-  <Fa icon={favorited ? solidHeart : faHeart} size="1.5x" />
-</div>
