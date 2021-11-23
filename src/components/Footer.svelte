04cfb9c1f731839303a286f452f101298ab956a7
Esky33 <djescaba@me.com>
2021-11-23 13:47:37 +0000

    updated footer links

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -7,7 +7,7 @@
     <div
       class="flex flex-col text-sm w-full lg:w-2/4 mb-10 order-last lg:order-first">
       <a href="/"><img src="/logo-white.svg" alt="logo" class="w-48" /></a>
-      <p class="mt-4">&copy;2021 {branding.projectName}</p>
+      <p class="mt-4">&copy;2021 JungleLab</p>
       <div class="flex items-center mt-12">
         <p class="pr-3">Powered by:</p>
         <img src="/liquid_logo.svg" alt="liquid network logo" />
@@ -16,9 +16,9 @@
     <div class="w-full lg:w-1/4 mb-10">
       <h4 class="font-bold mb-4">Follow US</h4>
       <ul class="list-none">
-        <li class="mb-2"><a target="_blank" href="{branding.urls.external.twitter}">Twitter</a></li>
+        <li class="mb-2"><a target="_blank" href="https://twitter.com/Esky33junglist">Twitter</a></li>
         <li class="mb-2"><a target="_blank" href="{branding.urls.external.telegram}">Telegram</a></li>
-        <li class="mb-2"><a target="_blank" href="{branding.urls.external.blog}">Blog</a></li>
+        <li class="mb-2"><a target="_blank" href="https://junglelab.net">Blog</a></li>
       </ul>
     </div>
     <div class="w-full lg:w-1/4 mb-10">
