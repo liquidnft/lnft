ed864d008cdd1ed66779633f28dec4a4773eb6cd
Adam Soltys <asoltys@gmail.com>
2021-11-26 14:05:53 -0800

    get metadata from a store

--- src/components/Head.svelte
+++ src/components/Head.svelte
@@ -1,9 +1,6 @@
 <script>
-  import branding from "$lib/branding";
-
-  const {
-    meta: { description, keywords, title, image, url, twitter },
-  } = branding;
+  import { meta as m } from "$lib/store";
+  const { description, keywords, title, image, video, url, twitter } = $m;
 </script>
 
 <svelte:head>
@@ -12,11 +9,19 @@
   <meta name="keywords" content={keywords} />
 
   <meta property="og:title" content={title} />
-  <meta property="og:image" content={image} />
-  <meta property="og:url" content={url} />
-
   <meta name="twitter:title" content={title} />
-  <meta name="twitter:image" content={image} />
+
+  {#if image}
+    <meta property="og:image" content={image} />
+    <meta name="twitter:image" content={image} />
+  {/if}
+
+  {#if video}
+    <meta property="og:video" content={video} />
+    <meta name="twitter:video" content={video} />
+  {/if}
+
+  <meta property="og:url" content={url} />
   <meta name="twitter:card" content={twitter.card} />
   <meta name="twitter:site" content={twitter.site} />
   <meta name="twitter:creator" content={twitter.creator} />
