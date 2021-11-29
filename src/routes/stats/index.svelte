cb7e2ee255a83a3538078351dda417de4bc72fe4
Zoltan <gyulainagyzoltan@gmail.com>
2021-11-29 17:47:53 +0200

    feat(stats): restricted access to admin users

---
+++ src/routes/stats/index.svelte
@@ -0,0 +1,28 @@
+<script>
+    import {getUserStats, getArtworksStats, getArtistsStats} from "$queries/stats";
+    import StatCard from './card.svelte';
+    import { page } from "$app/stores";
+    import { user } from "$lib/store";
+    import { goto } from "$lib/utils";
+    import { requireLogin } from "$lib/auth";
+
+    $: pageChange($page, $user);
+    let pageChange = async () => {
+        await requireLogin();
+        if (!$user) return;
+        if (!$user.is_admin) goto("/");
+    };
+</script>
+
+<style>
+</style>
+
+<div class="container mx-auto mt-20">
+    <h2 class="mb-10">User Stats</h2>
+
+    <div class="flex flex-wrap -mx-4">
+        <StatCard query={getUserStats} title="Number of users"/>
+        <StatCard query={getArtworksStats} title="Number of artworks"/>
+        <StatCard query={getArtistsStats} title="Number of artists"/>
+    </div>
+</div>
