eba27cc627ce679d519d823923d0678bcaeb7086
Esky33 <djescaba@icloud.com>
2021-12-01 13:43:43 +0000

    updated build.sh to pnpm from yarn

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 rm -rf build
-yarn build
-yarn adapt
+pnpm build
+pnpm adapt
 scp -r build la:~
 ssh la "rm -rf ~/la/hasura/data/raretoshi && mv build ~/la/hasura/data/raretoshi && docker restart nginx"
