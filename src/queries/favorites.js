26af8c97b6ef2410727f10f75e63c77ab9816e0b
Adam Soltys <asoltys@gmail.com>
2021-11-30 22:44:39 -0800

    fix favorites

--- src/queries/favorites.js
+++ src/queries/favorites.js
@@ -1,17 +1,13 @@
-export const createFavorite = {
-  query: `mutation insert_favorites_one($artwork_id: uuid!) {
-    insert_favorites_one(object: { artwork_id: $artwork_id }) {
-      user_id,
-      artwork_id
-    } 
-  }`,
-};
+export const createFavorite = `mutation insert_favorites_one($artwork_id: uuid!) {
+  insert_favorites_one(object: { artwork_id: $artwork_id }) {
+    user_id,
+    artwork_id
+  } 
+}`;
 
-export const deleteFavorite = {
-  query: `mutation delete_favorite($artwork_id: uuid!, $user_id: uuid!) {
-    delete_favorites_by_pk(artwork_id: $artwork_id, user_id: $user_id) {
-      user_id
-      artwork_id
-    } 
-  }`,
-};
+export const deleteFavorite = `mutation delete_favorite($artwork_id: uuid!, $user_id: uuid!) {
+  delete_favorites_by_pk(artwork_id: $artwork_id, user_id: $user_id) {
+    user_id
+    artwork_id
+  } 
+}`;
