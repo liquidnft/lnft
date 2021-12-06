23e18d5db0ae5337eab9a91a3630311d5533e8db
Adam Soltys <asoltys@gmail.com>
2021-12-05 22:45:46 -0800

    move user routes, create 404 page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,6 @@
+<div class="container mx-auto sm:justify-between mt-10 md:mt-20">
+  <div class="mx-auto">
+    <h2 class="mb-10">Page Not Found</h2>
+    <a href="/"> <button class="primary-btn"> Return to homepage</button></a>
+  </div>
+</div>
