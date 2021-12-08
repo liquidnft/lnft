851bc4398c223a1c002df81f6bb2b31c5c745b00
Gyulai Zoltan <gyulainagyzoltan@gmail.com>
2021-12-08 18:49:51 +0200

    fix: removed leftover code

--- hasura/migrations/default/1635219514839_run_sql_migration/up.sql
+++ hasura/migrations/default/1635219514839_run_sql_migration/up.sql
@@ -1,14 +1,12 @@
 CREATE
-OR REPLACE FUNCTION public.artwork_is_locked(artwork_row public.artworks, user_id uuid) returns boolean
+    OR REPLACE FUNCTION public.artwork_is_locked(artwork_row public.artworks, user_id uuid) returns boolean
     stable
     language sql
 as
 $function$
-SELECT key_is_owned IS NULL
-FROM (SELECT A.locked_by NOT IN
-             (SELECT edition_id FROM public.artworks B where B.owner_id = user_id)
-                 AND A.locked_by IS NOT NULL as key_is_owned
-      FROM public.artworks A
-      WHERE artwork_row.id = A.id
-     ) C;
+SELECT A.locked_by NOT IN
+       (SELECT edition_id FROM public.artworks B where B.owner_id = user_id)
+           AND A.locked_by IS NOT NULL as key_is_owned
+FROM public.artworks A
+WHERE artwork_row.id = A.id
 $function$
