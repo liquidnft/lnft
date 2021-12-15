4a3553d4ff9c4f5d4b19fce94a2703363ff30060
Adam Soltys <asoltys@gmail.com>
2021-12-15 01:26:28 -0800

    clear bid on transfer

---
+++ hasura/migrations/default/1639560363066_run_sql_migration/down.sql
@@ -0,0 +1,21 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE OR REPLACE FUNCTION public.trigger_set_transferred_at()
+--  RETURNS trigger
+--  LANGUAGE plpgsql
+-- AS $function$
+-- BEGIN
+--   IF NEW.owner_id <> OLD.owner_id THEN
+--     NEW.list_price = null;
+--     NEW.list_price_tx = null;
+--     NEW.auction_start = null;
+--     NEW.auction_end = null;
+--     NEW.max_extensions = null;
+--     NEW.extension_interval = null;
+--     NEW.reserve_price = null;
+--     NEW.bid_id = null;
+--     NEW.transferred_at = NOW();
+--   END IF;
+--   RETURN NEW;
+-- END;
+-- $function$;
