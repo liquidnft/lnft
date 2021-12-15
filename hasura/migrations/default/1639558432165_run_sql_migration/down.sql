52c5f80e03dfba4b17806e96d258a01d1a8d9171
Adam Soltys <asoltys@gmail.com>
2021-12-15 00:55:16 -0800

    fix update_bid function

---
+++ hasura/migrations/default/1639558432165_run_sql_migration/down.sql
@@ -0,0 +1,29 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE OR REPLACE FUNCTION public.trigger_update_bid()
+--  RETURNS trigger
+--  LANGUAGE plpgsql
+-- AS $function$
+-- DECLARE bid INTEGER;
+-- BEGIN
+--     IF NEW.type = 'bid' THEN
+--         SELECT transactions.amount
+--         FROM transactions
+--         JOIN artworks ON transactions.artwork_id = artworks.id
+--         WHERE transactions.artwork_id = NEW.artwork_id
+--         AND transactions.type = 'bid'
+--         AND transactions.id != NEW.id
+--         AND (transactions.created_at > artworks.transferred_at
+--         OR artworks.transferred_at IS NULL)
+--         ORDER BY amount DESC
+--         LIMIT 1
+--         INTO bid;
+--
+--       IF NEW.amount > bid THEN
+--         UPDATE artworks SET bid_id = NEW.id WHERE id = NEW.artwork_id;
+--       END IF;
+--     END IF;
+--
+--   RETURN NEW;
+-- END;
+-- $function$;
