945216d1c2d7e0ae62e0d1f7b064a675d86fc22c
Marius Buraga <marius.buraga89@yahoo.com>
2021-12-17 16:55:43 +0200

    Ref: RARE-45

--- hasura/app/mail.js
+++ hasura/app/mail.js
@@ -19,29 +19,195 @@ mail = new Email({
   message: { from: SMTP_SENDER },
   send: true,
   views: {
-    root: path.resolve(process.env.PWD || ".", "../custom/emails"),
+    // root: path.resolve(process.env.PWD || ".", "../custom/emails"),
     options: {
       extension: "ejs",
     },
   },
 });
 
-app.post("/notify-outbid", async (req, res) => {
+app.post("/mail-artist-application-approved", auth, async (req, res) => {
   try {
+    const { artistName, to } = req.body;
+
     await mail.send({
-      template: "notify-outbid",
+      template: "artist-application-approved",
       locals: {
-        potato: "fries",
+        artistName,
       },
       message: {
-        to: "asoltys@gmail.com",
+        to,
       },
     });
-  } catch (err) {
-    console.error("Unable to send email");
-    console.error(err);
-    return res.code(204).send();
-  }
 
-  res.send("ok");
+    return res.send("ok");
+  } catch (err) {
+    console.error(err);
+    return res.code(400).send();
+  }
+});
+
+app.post("/mail-artist-application-denied", auth, async (req, res) => {
+  try {
+    const { artistName, to } = req.body;
+
+    await mail.send({
+      template: "artist-application-denied",
+      locals: {
+        artistName,
+      },
+      message: {
+        to,
+      },
+    });
+
+    return res.send("ok");
+  } catch (err) {
+    console.error(err);
+    return res.code(400).send();
+  }
+});
+
+app.post("/mail-bid-processed", auth, async (req, res) => {
+  try {
+    const { userName, to, artworkUrl, artworkTitle, bidAmount } = req.body;
+
+    await mail.send({
+      template: "bid-processed",
+      locals: {
+        userName,
+        bidAmount,
+        artworkTitle,
+        artworkUrl,
+      },
+      message: {
+        to,
+      },
+    });
+
+    return res.send("ok");
+  } catch (err) {
+    console.error(err);
+    return res.code(400).send();
+  }
+});
+
+app.post("/mail-outbid", auth, async (req, res) => {
+  try {
+    const { userName, to, artworkUrl, artworkTitle, bidAmount } = req.body;
+
+    await mail.send({
+      template: "outbid",
+      locals: {
+        userName,
+        bidAmount,
+        artworkTitle,
+        artworkUrl,
+      },
+      message: {
+        to,
+      },
+    });
+
+    return res.send("ok");
+  } catch (err) {
+    console.error(err);
+    return res.code(400).send();
+  }
+});
+
+app.post("/mail-purchase-successful", auth, async (req, res) => {
+  try {
+    const { userName, to, artworkUrl, artworkTitle, bidAmount } = req.body;
+
+    await mail.send({
+      template: "purchase-successful",
+      locals: {
+        userName,
+        bidAmount,
+        artworkTitle,
+        artworkUrl,
+      },
+      message: {
+        to,
+      },
+    });
+
+    return res.send("ok");
+  } catch (err) {
+    console.error(err);
+    return res.code(400).send();
+  }
+});
+
+app.post("/mail-artwork-minted", auth, async (req, res) => {
+  try {
+    const { userName, to, artworkUrl, artworkTitle, bidAmount } = req.body;
+
+    await mail.send({
+      template: "artwork-minted",
+      locals: {
+        userName,
+        artworkTitle,
+        artworkUrl,
+      },
+      message: {
+        to,
+      },
+    });
+
+    return res.send("ok");
+  } catch (err) {
+    console.error(err);
+    return res.code(400).send();
+  }
+});
+
+app.post("/mail-someone-bid", auth, async (req, res) => {
+  try {
+    const { userName, to, artworkUrl, artworkTitle, bidAmount } = req.body;
+
+    await mail.send({
+      template: "someone-bid",
+      locals: {
+        userName,
+        bidAmount,
+        artworkTitle,
+        artworkUrl,
+      },
+      message: {
+        to,
+      },
+    });
+
+    return res.send("ok");
+  } catch (err) {
+    console.error(err);
+    return res.code(400).send();
+  }
+});
+
+app.post("/mail-artwork-sold", auth, async (req, res) => {
+  console.log("auth", auth)
+  try {
+    const { userName, to, artworkUrl, artworkTitle, bidAmount } = req.body;
+
+    await mail.send({
+      template: "artwork-sold",
+      locals: {
+        userName,
+        bidAmount,
+        artworkTitle,
+        artworkUrl,
+      },
+      message: {
+        to,
+      },
+    });
+
+    return res.send("ok");
+  } catch (err) {
+    console.error(err);
+    return res.code(400).send();
+  }
 });
