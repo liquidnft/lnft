a1ab69f6baa44b3d2fcb294eee5037ee16e9b558
Adam Soltys <asoltys@gmail.com>
2021-12-26 23:30:15 -0800

    disable analytics in dev mode

--- src/components/Head.svelte
+++ src/components/Head.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { dev } from "$lib/utils";
   export let metadata;
 </script>
 
@@ -27,4 +28,35 @@
   <meta name="twitter:card" content={metadata.twitter.card} />
   <meta name="twitter:site" content={metadata.twitter.site} />
   <meta name="twitter:creator" content={metadata.twitter.creator} />
+
+  {#if !dev}
+  <script
+    async
+    src="https://www.googletagmanager.com/gtag/js?id=G-DJNPT2CQXL"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+
+    gtag("config", "G-DJNPT2CQXL");
+  </script>
+
+  <script>
+    (function (h, o, t, j, a, r) {
+      h.hj =
+        h.hj ||
+        function () {
+          (h.hj.q = h.hj.q || []).push(arguments);
+        };
+      h._hjSettings = { hjid: 2706575, hjsv: 6 };
+      a = o.getElementsByTagName("head")[0];
+      r = o.createElement("script");
+      r.async = 1;
+      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+      a.appendChild(r);
+    })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
+  </script>
+{/if}
 </svelte:head>
