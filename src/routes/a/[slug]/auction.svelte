04879378d4748afd88dbc1eaa513ca178f987179
Esky33 <djescaba@icloud.com>
2021-12-31 14:46:10 +0000

    check env & cypress json

--- src/routes/a/[slug]/auction.svelte
+++ src/routes/a/[slug]/auction.svelte
@@ -459,15 +459,15 @@
   input[type="checkbox"]:checked {
     appearance: none;
     border: 5px solid #fff;
-    outline: 2px solid #6ed8e0;
-    background-color: #6ed8e0;
+    outline: 2px solid #43470B;
+    background-color: #43470B;
     padding: 2px;
     border-radius: 0;
   }
 
   input[type="radio"]:checked {
     appearance: none;
-    border: 7px solid #6ed8e0;
+    border: 7px solid #43470B;
     background-color: #fff;
     padding: 2px;
     border-radius: 100%;
