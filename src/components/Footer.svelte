186502a74068ac29539e917f718d265a3a15cfd3
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-01-20 20:02:37 -0700

    Update footer help links

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -88,12 +88,13 @@
         <li class="mb-2">
           <a href="https://blockstream.com/liquid/">The Liquid Network</a>
         </li>
+				<li class="mb-2"><a href="/guide">User Guide</a></li>
+				<li class="mb-2"><a href="/faq">FAQs</a></li>
         <li class="mb-2"><a href="mailto:support@raretoshi">Support</a></li>
         <li class="mb-2"><a href="/privacy-policy">Privacy Policy</a></li>
         <li class="mb-2">
           <a href="/terms-and-conditions">Terms of Service</a>
         </li>
-        <li class="mb-2"><a href="/faq">FAQs</a></li>
       </ul>
     </div>
   </div>
