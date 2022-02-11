a9a9092053f37b1ab24ee5127d21740d32e53e69
Adam Soltys <asoltys@gmail.com>
2022-02-10 20:38:05 -0800

    cascade delete royalties

---
+++ hasura/migrations/default/1644554269431_set_fk_public_royalty_recipients_artwork_id/down.sql
@@ -0,0 +1,5 @@
+alter table "public"."royalty_recipients" drop constraint "royalty_recipients_artwork_id_fkey",
+  add constraint "royalty_recipients_artwork_id_fkey"
+  foreign key ("artwork_id")
+  references "public"."artworks"
+  ("id") on update restrict on delete restrict;
