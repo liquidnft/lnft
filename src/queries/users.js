94e846523cec12880f40543a3adfaa10d597d140
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-05-04 16:13:17 -0600

    remove externally held tokens from collections tab

--- src/queries/users.js
+++ src/queries/users.js
@@ -10,56 +10,56 @@ let privateFields = "mnemonic, wallet_initialized, is_admin, info, has_samples";
 let computed = "followed, num_follows, num_followers";
 
 export const getUser = `query {
-  currentuser (limit: 1) { 
-    ${fields} 
+  currentuser (limit: 1) {
+    ${fields}
     ${privateFields}
   }
 }`;
 
 export const getUserByUsername = `query($username: String!, $artworksLimit: Int) {
-  users(where: { username: {_eq: $username }}, limit: 1) { 
-    ${fields} 
+  users(where: { username: {_eq: $username }}, limit: 1) {
+    ${fields}
     ${computed}
-    holdings(limit: $artworksLimit, order_by: { created_at: desc }) {
-      ${artworkFields} 
-    } 
+    holdings(where: { held: {_is_null: false }}, limit: $artworksLimit, order_by: { created_at: desc }) {
+      ${artworkFields}
+    }
     creations(limit: $artworksLimit, order_by: { created_at: desc }) {
-      ${artworkFields} 
-    } 
+      ${artworkFields}
+    }
     offers {
       transaction {
         ${txFields}
         artwork {
           ${artworkFields}
-        } 
+        }
       }
-    } 
+    }
     activebids {
       transaction {
         ${txFields}
         artwork {
           ${artworkFields}
-        } 
+        }
       }
     }
     favorites {
       artwork {
         ${artworkFields}
       }
-    } 
+    }
   }
 }`;
 
 export const getSamples = `query {
   users(where: { _and: [{ is_artist: { _eq: false }}, { samples: {}}]}) {
     display_name
-    ${fields} 
+    ${fields}
     info
     samples {
       id
       url
       type
-    } 
+    }
   }
 }`;
 
@@ -80,20 +80,20 @@ export const deleteSamples = `mutation deleteSamplesByUserId($user_id: uuid!) {
 }`;
 
 export const topCollectors = (limit) => `query {
-  collectors(limit: ${limit}) { 
+  collectors(limit: ${limit}) {
     id
     username
     avatar_url
     owned
     collected
     resold
-    avg_price	
+    avg_price
     total_price
   }
 }`;
 
 export const topArtists = (limit) => `query {
-  artists(limit: ${limit}) { 
+  artists(limit: ${limit}) {
     id
     username
     avatar_url
